a1e2afda4eed59ec67831bdfdfa30a8c932bd2c2
niceynice1221 <bunthita6677@gmail.com>
2023-08-16 17:46:35 +0700

    'finishedch04'

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express')
+let bodyParser =require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/status', function (req,res){
     res.send('Hello nodejs server belong to bunthita')
@@ -10,6 +15,25 @@ app.get('/hello/:name', function(req,res){
     res.send('say hello with ' + req.params.name)
 })
 
+app.get('/user/:userid', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้'+req.params.userid)
+})
+
+app.get('/users', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+})
+
+app.post('/user',function (req,res){
+    res.send('สร้างผู้ใช้'+JSON.stringify(req.body))
+})
+
+app.put('/user/:userid', function(req,res){
+    res.send('แก้ไขข้อมูลผู้ใช้'+req.params.userid + JSON.stringify(req.body.name))
+})
+
+app.delete('/user/:userid', function(req,res){
+    res.send('ลบข้อมูลผู้ใช้'+req.params.userid + JSON.stringify(req.body.name))
+})
 let port = 8081
 app.listen(port, function(){
     console.log('server runing on ' + port)
